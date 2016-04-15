1e90f7f381f4803ec35f24c606b327bc74a02766
Alejandro Garrido <alejandrogarrido@Alejandros-MacBook-Air.local>
2016-04-15 14:40:49 -0500

    working needs exit

--- highlow.php
+++ highlow.php
@@ -1,3 +1,29 @@
 <?php
 
 $randomNum = rand(1, 100);
+
+fwrite(STDOUT, "Take a guess between 1-100\n");
+ echo $randomNum.PHP_EOL;
+ echo $guess.PHP_EOL;
+
+
+do {
+    $guess = fgets(STDIN);
+    if ($guess == $randomNum) {
+        echo "somehow...you cheated\n";
+    } elseif ($guess > $randomNum) {
+        echo "too high\n";
+    } elseif ($guess < $randomNum) {
+        echo "too low\n";
+    }
+    $hint = $randomNum - $guess;
+    if ($hint < 10 && $hint > -10) {
+        echo "hot\n";
+    } else {
+        echo "cold\n";
+    }
+    if ($guess == 'exit') {
+        `exit`;
+    }
+} while ($guess != $randomNum);
+    
